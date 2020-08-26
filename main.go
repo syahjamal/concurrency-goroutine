0979e77aa7113168cee5e1a19dd932a5330c15e5
syahjamal <sulaimansyahjamal@gmail.com>
2020-08-26 13:47:25 +0700

    go routine dengan 2 fungsi berbeda

--- main.go
+++ main.go
@@ -5,18 +5,27 @@ import (
 	"time"
 )
 
-func printSalam(text string) {
+func printAngka() {
 	for i := 0; i < 5; i++ {
-		time.Sleep(100 * time.Millisecond)
-		fmt.Println(text)
+		time.Sleep(200 * time.Millisecond)
+		fmt.Println(i)
+	}
+}
+
+func printText() {
+	for i := 0; i < 5; i++ {
+		time.Sleep(200 * time.Millisecond)
+		fmt.Println("text ", i)
 	}
 }
 
 func main() {
 	start := time.Now() //cel kecepatan proses
 
-	go printSalam("Selamat Datang !") //goroutine ditandai dengan code go di awal func
-	printSalam("Selamat Jalan")
+	go printAngka()
+	go printText()
+
+	time.Sleep(1000 * time.Millisecond)
 
 	fmt.Println(time.Since(start)) //cek kecepatan proses
 }
