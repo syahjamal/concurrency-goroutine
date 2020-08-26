dac43d710ffcfa2f8101cc126e481178fce662d6
syahjamal <sulaimansyahjamal@gmail.com>
2020-08-26 14:23:38 +0700

    update

--- main.go
+++ main.go
@@ -6,13 +6,16 @@ import (
 )
 
 //Declare global itemschannel
-var itemsChannel = make(chan string)
+var itemsChannel = make(chan string) //channel 1
+
+var cleanedItemsChannel = make(chan string) //channel 2
 
 func main() {
 	items := [7]string{"batu", "harta", "kerang", "harta", "batu", "kerang"}
 
 	go menyelam(items)
 	go membersihkan()
+	go menyimpan()
 
 	time.Sleep(500 * time.Millisecond)
 }
@@ -29,6 +32,13 @@ func menyelam(items [7]string) {
 func membersihkan() {
 	for rawItem := range itemsChannel {
 		fmt.Println("berhasil membersihkan " + rawItem)
+		cleanedItemsChannel <- "hartaBersih"
+	}
+}
+
+func menyimpan() {
+	for cleanedItem := range cleanedItemsChannel {
+		fmt.Println("Berhasil menyimpan " + cleanedItem)
 	}
 }
 
